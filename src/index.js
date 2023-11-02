d650c998160aa5dcffa574967211081302460de1
sgc93 <smachewgedefaw@gmail.com>
2023-11-02 01:36:53 -0900

    use dynamic list rendering

--- src/index.js
+++ src/index.js
@@ -10,13 +10,34 @@ const data = {
 	aboutMe:
 		"As a front-end developer, I find joy in the process of turning design concepts into interactive and responsive digital experiences, ensuring that the end product not only looks great but also functions seamlessly.",
 	myTechs: [
-		"Web Design",
-		"Product Design",
-		"Portfolio Development",
-		"Mobile App Development",
-		"Web Re-Design",
-		"Adobe Illustrator",
-		"Adobe Photoshop",
+		{
+			name: "Web Design",
+			color: "rgb(38, 74, 106)",
+		},
+		{
+			name: "Product Design",
+			color: "green",
+		},
+		{
+			name: "Portfolio Development",
+			color: "rgb(137, 89, 0)",
+		},
+		{
+			name: "Mobile App Development",
+			color: "rgb(38, 74, 106)",
+		},
+		{
+			name: "Web Re-Design",
+			color: "green",
+		},
+		{
+			name: "Adobe Tools",
+			color: "rgb(137, 89, 0)",
+		},
+		{
+			name: "Figma",
+			color: "rgb(38, 74, 106)",
+		},
 	],
 };
 
@@ -78,22 +99,16 @@ function About(props) {
 function Techs(props) {
 	return (
 		<div className="techs">
-			<p className={`techs-${0}`}>{props.techs[0]}</p>
-			<p className={`techs-${1}`}>{props.techs[1]}</p>
-			<p className={`techs-${0}`}>{props.techs[2]}</p>
-			<p className={`techs-${2}`}>{props.techs[3]}</p>
-			<p className={`techs-${0}`}>{props.techs[4]}</p>
-			<p className={`techs-${1}`}>{props.techs[5]}</p>
-			<p className={`techs-${2}`}>{props.techs[6]}</p>
+			{props.techs.map((tech) => {
+				let bgColor = { backgroundColor: tech.color };
+				return (
+					<p className="tech" style={bgColor}>
+						{tech.name}
+					</p>
+				);
+			})}
 		</div>
 	);
-}
-
-function addTech(techLIst) {
-	const techDiv = document.querySelector(".techs");
-	for (var i = 0; i < techLIst.length; i++) {
-		techDiv.appendChild = <p className={`thech-${i}`}>{techLIst[i]}</p>;
-	}
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
